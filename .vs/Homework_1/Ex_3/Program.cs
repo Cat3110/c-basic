dedb25cbfd42d9325257bf4bbec1d772719dcea3
Catello <egor@vasilyev.sexy>
2020-09-26 12:39:54 +0300

    Three exersizes was finished

---
+++ .vs/Homework_1/Ex_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+// Васильев Егор - Домашнее задание №1 - Задача №3
+
+// а) Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2, y2 по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2 - y1, 2).
+// Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
+// б) *Выполнить предыдущее задание, оформив вычисления расстояния между точками в виде метода.
+
+
+namespace Ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program \"Distance\"");
+
+            // Вводим координаты первой точки
+            Console.WriteLine("Enter first point coordinates");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            // Вводим координаты второй точки
+            Console.WriteLine("Enter second point coordinates");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            // Считаем расстояние между точками
+            double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("Distance between points = {0:F2}", r);
+
+            Console.WriteLine("Distance throw using method = {0:F2}", Distance(x1, y1, x2, y2));
+
+        }
+
+        static double Distance(double x1,double y1,double x2,double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+    }
+}
---
+++ .vs/Homework_1/Ex_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Васильев Егор - Домашнее задание №1 - Задача №3
+
+// Написать программу обмена значениями двух переменных:
+// а) с использованием третьей переменной;
+// б) *без использования третьей переменной.
+
+namespace Ex_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program \"Exchange\"");
+
+            Console.WriteLine("Enter first variable");
+            string a = Console.ReadLine();            
+
+            Console.WriteLine("Enter second variable");
+            string b = Console.ReadLine();
+
+            // С использованием третьей переменной
+            string temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            // Без использования третьей переменной
+            a = a  b;
+            b = b ^ a;
+            a = a ^ b;
+
+
+
+
+        }
+    }
+}
