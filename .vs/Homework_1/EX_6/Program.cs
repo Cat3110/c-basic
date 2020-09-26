f1ec950329b6f07e98ca635e8e6bac55510b5dff
Catello <egor@vasilyev.sexy>
2020-09-26 12:51:58 +0300

    Ex6 was added

---
+++ .vs/Homework_1/EX_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// *Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+
+namespace EX_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    class Utilities
+    {
+        static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
+    }
+}
