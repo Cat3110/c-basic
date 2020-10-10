da92a9d88784a9efd58aefbaa0196c405e0d90f0
Egor Vasilyev <egor@vasilyev.sexy>
2020-10-10 12:36:25 +0300

    First and third ex was finished

---
+++ Homework_5/Ex_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+// Васильев Егор - Домашная работа №5 - Задание 1.
+
+// Создать программу, которая будет проверять корректность ввода логина. 
+// Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+// а) без использования регулярных выражений;
+// б) **с использованием регулярных выражений.
+
+namespace Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login;
+            // Цикл для ожидания ввода корректного логина
+            do
+            {
+                Console.WriteLine("Enter login");
+                login = Console.ReadLine();
+            } while (!CheckCorrectWithRegular(login));
+            Console.WriteLine($"Login: {login} is correct");
+        }
+
+        // Проверка корректности без регулярных выражений
+        static bool CheckCorrect(string login)
+        {
+            // Проверяем что не короче 2 символов
+            if (login.Length < 2) 
+                return false;
+            // Проверяем что не длиннее 10 символов
+            if (login.Length > 10) 
+                return false;
+            // Проверяем что цифра не первая
+            if (Char.IsDigit(login[0]))
+                return false;
+            // Проверяем, что только буквы и цифры
+            for (int i = 0; i < login.Length; i++)
+            {
+                if(!Char.IsLetterOrDigit(login[i]))
+                    return false;
+            }
+            return true;
+        }
+        // Проверка корректности с регулярными выражениями
+        static bool CheckCorrectWithRegular(string login)
+        {
+            Regex myReg = new Regex(@"^[a-zA-Z]{1}\w{1,9}$");
+            if (myReg.IsMatch(login))
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Homework_5/Ex_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// Васильев Егор - Домашная работа №5 - Задание 1.
+
+// *Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+// Например:
+// badc являются перестановкой abcd.
+
+namespace Ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first and second strings");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            Console.WriteLine(Permutation(a, b));
+        }
+
+        static bool Permutation(string a, string b)
+        {
+            // Сравниваем длины строк
+            if (a.Length != b.Length)
+                return false;
+
+            // Считаем количество символов в первой строке
+            int[] letters = new int[256];
+            char[] sArray = a.ToCharArray();
+            foreach (char c in sArray)
+            {
+                letters[c]++;
+            }
+
+            // Проверяем на перестановку
+            for (int i = 0; i < b.Length; i++)
+            {
+                int c = (int)b[i];
+                if (--letters[c] < 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
